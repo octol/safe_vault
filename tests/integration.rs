1c1f4ee037ac9f2bb46078f74a9b448c3dff8b53
Jon Haggblad <jon.haggblad@gmail.com>
2019-08-28 10:47:42 +0200

    fix/client_handler: DeleteMData should be free of charge
    - Make DeleteMData free of charge
    - Rename `handle_mdata_mutation` to `handle_mutate_mdata` to fit the
      pattern used by other functions
    - `handle_delete_mdata()` and `handle_get_mdata()` are now the same,
      however I'm not convinced to makes sense to use the same function for
      both of these cases.

--- tests/integration.rs
+++ tests/integration.rs
@@ -2829,6 +2829,8 @@ fn delete_mutable_data() {
         &mut client_a,
         Request::PutMData(MData::Unseq(mdata.clone())),
     );
+    let balance_a = unwrap!(balance_a.checked_sub(*COST_OF_PUT));
+    common::send_request_expect_ok(&mut env, &mut client_a, Request::GetBalance, balance_a);
 
     // Attempt to delete non-existent data.
     let invalid_address = MDataAddress::from_kind(MDataKind::Unseq, env.rng().gen(), 101);
@@ -2838,6 +2840,7 @@ fn delete_mutable_data() {
         Request::DeleteMData(invalid_address),
         NdError::NoSuchData,
     );
+    common::send_request_expect_ok(&mut env, &mut client_a, Request::GetBalance, balance_a);
 
     // Attempt to delete the data by non-owner.
     common::send_request_expect_err(
@@ -2846,9 +2849,11 @@ fn delete_mutable_data() {
         Request::DeleteMData(address),
         NdError::AccessDenied,
     );
+    common::send_request_expect_ok(&mut env, &mut client_a, Request::GetBalance, balance_a);
 
     // Successfully delete.
     common::send_request_expect_ok(&mut env, &mut client_a, Request::DeleteMData(address), ());
+    common::send_request_expect_ok(&mut env, &mut client_a, Request::GetBalance, balance_a);
 
     // Verify the data doesn't exist any more.
     common::send_request_expect_err(
